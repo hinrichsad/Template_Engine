cc5bb7dbc4b2b0b2cd57df1c39c4fbfad3f0acb5
Andy Hinrichs <hinrichsad@gmail.com>
2021-01-26 11:06:44 -0500

    set up skeleton

---
+++ app.js
@@ -0,0 +1,12 @@
+const manager = require("./lib/manager");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
